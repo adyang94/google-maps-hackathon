383f8bacfd3cde14ea4236d95a753c55d9406bb1
w3tsa <tanveersayem@gmail.com>
2022-09-25 12:08:52 -0400

    made some changes to get some suggestions when user put in an destination :tada:

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import "./App.css";
 // local imports
 import Map from "./components/Map";
 function App() {
-  return (
-    <div className="App">
-      <Map />
-      <form></form>
-    </div>
-  );
+  return <Map />;
 }
 
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,48 +1,83 @@
-import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { useMemo, useRef, memo, useCallback, useState } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  DirectionsRenderer,
+  Circle,
+  MarkerClusterer,
+} from "@react-google-maps/api";
+
+// local imports
+import Places from "./Places";
 
 const containerStyle = {
-  width: "100vw",
+  width: "100%",
   height: "100vh",
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.524,
-};
-
 function Map() {
+  const [destination, setDestination] = useState();
+  const mapRef = useRef();
+  const center = useMemo(
+    () => ({
+      lat: -3.745,
+      lng: -38.524,
+    }),
+    []
+  );
+  const options = useMemo(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_API_KEY,
+    libraries: ["places"],
   });
 
-  const [map, setMap] = React.useState(null);
+  const [map, setMap] = useState(null);
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    setMap(map);
-  }, []);
+  // const onLoad = useCallback(function callback(map) {
+  //   const bounds = new window.google.maps.LatLngBounds(center);
+  //   map.fitBounds(bounds);
+  //   setMap(map);
+  // }, []);
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
 
-  const onUnmount = React.useCallback(function callback(map) {
+  const onUnmount = useCallback(function callback(map) {
     setMap(null);
   }, []);
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
+    <div className="container">
+      <aside>
+        <h1>Controls</h1>
+        <Places
+          setDestination={(position) => {
+            setDestination(position);
+            mapRef.current?.panTo(position);
+          }}
+        />
+      </aside>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={options}
+      >
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </div>
   ) : (
-    <></>
+    <> Loading...</>
   );
 }
 
-export default React.memo(Map);
+export default memo(Map);
