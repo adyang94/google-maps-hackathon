b6c0eeeb3318c1850496645dd773b1c42abc0f4c
adyang94 <adyang94@yahoo.com>
2022-09-25 13:18:35 -0400

    fixing merge

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 // local imports
 import Map from "./components/Map";
 function App() {
-<<<<<<< HEAD
   return (
     <div className="App">
       <Map />
@@ -11,9 +10,6 @@ function App() {
 
     </div>
   );
-=======
-  return <Map />;
->>>>>>> 383f8bacfd3cde14ea4236d95a753c55d9406bb1
 }
 
 export default App;
